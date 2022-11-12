3bcd4281a9e1c88dbb973b536f2aac5dd42cba1d
Relieved <424015201@qq.com>
2022-11-12 16:21:06 +0800

    配置调整

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
+  outputDir: "docs",
+  publicPath: "./",
+  baseUrl: "./",
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
